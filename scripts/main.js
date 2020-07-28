529166faa19071ba438ed26467b13a3ae712c020
mohsinht <23508720+mohsinht@users.noreply.github.com>
2020-07-28 23:25:19 +0500

    Add header state 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,26 @@
 $(document).ready(function () {
-  console.log('WORKING!');
   $(window).scroll(function () {
     if ($(this).scrollTop() > 70) {
       $('#header-logo').attr('src', 'assets/logo-2018.svg');
     } else {
       $('#header-logo').attr('src', 'assets/logo_white.png');
     }
+  });
+
+  $('#nav-menu').hover(function () {
+    console.log('working?');
+    $('.header').addClass('solid');
+    $('.header').removeClass('transparent');
+    $('.menu-wrapper').addClass('solid');
+    $('.menu-wrapper').removeClass('transparent');
+    $('#header-logo').attr('src', 'assets/logo-2018.svg');
+  });
+
+  $('#nav-menu').mouseleave(function () {
+    $('.header').addClass('transparent');
+    $('.header').removeClass('solid');
+    $('.menu-wrapper').removeClass('solid');
+    $('.menu-wrapper').addClass('transparent');
+    $('#header-logo').attr('src', 'assets/logo_white.png');
   });
 });
