68db8add1a476579586509c29295cab178ba0670
mohsinht <23508720+mohsinht@users.noreply.github.com>
2020-07-29 22:51:51 +0500

    add mega menu

--- scripts/main.js
+++ scripts/main.js
@@ -24,3 +24,21 @@ $(document).ready(function () {
     $('#header-logo').attr('src', 'assets/logo_white.png');
   });
 });
+
+// breakpoint and up
+$(window).resize(function () {
+  if ($(window).width() >= 980) {
+    // when you hover a toggle show its dropdown menu
+    $('.navbar .dropdown-toggle').hover(function () {
+      $(this).parent().toggleClass('show');
+      $(this).parent().find('.dropdown-menu').toggleClass('show');
+    });
+
+    // hide the menu when the mouse leaves the dropdown
+    $('.navbar .dropdown-menu').mouseleave(function () {
+      $(this).removeClass('show');
+    });
+
+    // do something here
+  }
+});
