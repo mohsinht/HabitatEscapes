eabce0b12c3ec15313b1c45c633bc96ff10b0996
mohsinht <23508720+mohsinht@users.noreply.github.com>
2020-07-31 01:33:47 +0500

    filter menu structure

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 70) {
+    if ($(this).scrollTop() > 750) {
       $('#header-logo').attr('src', 'assets/logo-2018.svg');
+      $('#filter-header').removeClass('d-none');
     } else {
       $('#header-logo').attr('src', 'assets/logo_white.png');
+      $('#filter-header').addClass('d-none');
     }
   });
 
   $('#nav-menu').hover(function () {
-    console.log('working?');
     $('.header').addClass('solid');
     $('.header').removeClass('transparent');
     $('.menu-wrapper').addClass('solid');
