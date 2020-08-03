0d7a03244c497386168c0d25ff768be0b18f52e8
Kashif Hussain <logiccanvas@gmail.com>
2020-08-04 04:15:46 +0500

    property detail and activity pages sliders implementation

--- scripts/main.js
+++ scripts/main.js
@@ -30,20 +30,38 @@ $(document).ready(function () {
   if ( $('.slider-synced').length ) {
 
     $('.slider-synced').each(function() {
-      var sliderFor = $(this).data("for");
-      var sliderNav = $(this).data("nav");
+      
+      var sliderFor = $(this).data("for"),
+          sliderNav = $(this).data("nav"),
+          arrowsMain = $(this).data("arrows-main"),
+          arrowsNav = $(this).data("arrows-nav"),
+          navCount = $(this).data("nav-count"),
+          fade = $(this).data("fade");
+
+    if ( $('.slider-thumbs-count').length ) {
+      var navbar = $(sliderNav)[0],
+          list = $(navbar.children);
+          
+          $('.slider-thumbs-count .count').text(list.length);
+      }
+
+      arrowsMain = arrowsMain ?? false;
+      arrowsNav = arrowsNav ?? false;
+      navCount = navCount ?? 3;
+      fade = fade ?? false;
         
       $(sliderFor).slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false,
-        fade: true,
+        arrows: arrowsMain,
+        fade: fade,
         asNavFor: sliderNav
       });
       $(sliderNav).slick({
-        slidesToShow: 3,
+        slidesToShow: navCount,
         slidesToScroll: 1,
         asNavFor: sliderFor,
+        arrows: arrowsNav,
         dots: false,
         centerMode: false,
         focusOnSelect: true
